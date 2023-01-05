bb1409d7353af8b4034795644bcd5f2af34d9b4f
William Adjei Jr <williamadjeijunior@gmail.com>
2023-01-05 02:56:23 -0800

    Python

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 1-rectangle.py
 """Defines a Rectangle class."""
 
 
@@ -39,4 +38,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = 
+        self.__height = value
